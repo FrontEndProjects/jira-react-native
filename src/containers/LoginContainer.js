7f8f513bdda271d3ef43da336987547ea27110ed
irekrog <irekfrontend@gmail.com>
2016-11-01 16:39:00 +0100

    add custom theme

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -3,13 +3,15 @@ import LoginForm from '../components/form/LoginForm';
 
 import { Container, Content, Header, Title } from 'native-base';
 
+import myTheme from '../../Themes/myTheme';
+
 export default class LoginContainer extends Component {
 
   render () {
     return (
       <Container>
         <Content>
-          <Header>
+          <Header theme={myTheme}>
             <Title>Log In to Jira</Title>
           </Header>
           <LoginForm
