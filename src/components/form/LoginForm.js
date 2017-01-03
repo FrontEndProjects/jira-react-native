6e15675199fce8b8db2ec226ca1f9ed7e125a6d0
Filip Bialek <bialek.f@gmail.com>
2017-01-03 11:23:19 +0100

    change loginform to pure function

--- src/components/form/LoginForm.js
+++ src/components/form/LoginForm.js
@@ -2,32 +2,31 @@ import React, {Component} from 'react';
 
 import {Content, InputGroup, Input, Button} from 'native-base';
 
-export default class LoginForm extends Component {
-
-  render () {
-    return (
-      <Content>
-        <InputGroup>
-          <Input
-            placeholder="Enter your login"
-            onChangeText={this.props.handleLoginInput}
-          />
-        </InputGroup>
-        <InputGroup>
-          <Input
-            placeholder="Enter your password"
-            secureTextEntry={true}
-            onChangeText={this.props.handlePasswordInput}
-          />
-        </InputGroup>
-        <InputGroup>
-          <Input
-            placeholder="Enter your link"
-            onChangeText={this.props.handleLoginInput}
-          />
-        </InputGroup>
-        <Button block primary onPress={this.props.handleLoginButton}>Login</Button>
-      </Content>
-    );
-  }
+function LoginForm(props) {
+  return (
+    <Content>
+      <InputGroup>
+        <Input
+          placeholder="Enter your login"
+          onChangeText={props.handleLoginInput}
+        />
+      </InputGroup>
+      <InputGroup>
+        <Input
+          placeholder="Enter your password"
+          secureTextEntry={true}
+          onChangeText={props.handlePasswordInput}
+        />
+      </InputGroup>
+      <InputGroup>
+        <Input
+          placeholder="Enter your link"
+          onChangeText={props.handleLoginInput}
+        />
+      </InputGroup>
+      <Button block primary onPress={props.handleLoginButton}>Login</Button>
+    </Content>
+  );
 }
+
+export default LoginForm
