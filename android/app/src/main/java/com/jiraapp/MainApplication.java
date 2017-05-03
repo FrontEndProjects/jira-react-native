d8e88402176122e185cf284e648f691375d0eae8
irekrog <irekfrontend@gmail.com>
2017-05-03 14:25:10 +0200

    fix react-native link

--- android/app/src/main/java/com/jiraapp/MainApplication.java
+++ android/app/src/main/java/com/jiraapp/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
+import com.evollu.react.fcm.FIRMessagingPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ReactNativeLocalizationPackage(),
+            new FIRMessagingPackage(),
           new FIRMessagingPackage()
       );
     }
